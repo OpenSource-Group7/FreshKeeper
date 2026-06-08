524576f1afcfa18a0f27bb72bcff6ff0251a8655
kimdagyeongg <dk32250410@dankook.ac.kr>
2026-06-08 16:24:13 +0900

    feat: 단위 변환 함수(convert_units) 구현

--- InventoryManager.py
+++ InventoryManager.py
@@ -12,4 +12,14 @@ class InventoryManager:
             "port": "5432",
         }
 
-   
+    # 단위 변환 함수
+    def convert_units(self, row):
+        name = row["ingredient_name"]
+        unit = row["unit"]
+        qty = row["quantity"]
+        
+        if unit == "kg":
+            return pd.Series([qty * 1000.0, "g"])
+        elif unit == "L":
+            return pd.Series([qty * 1000.0, "ml"])
+        return pd.Series([qty, unit])
