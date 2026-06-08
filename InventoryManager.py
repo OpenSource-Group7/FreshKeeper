506f874cf290cc3a52093a1e8c0ca7556a7b666e
kimdagyeongg <dk32250410@dankook.ac.kr>
2026-06-08 16:24:45 +0900

    feat: 재고 조회 및 유통기한 필터링 구현

--- InventoryManager.py
+++ InventoryManager.py
@@ -23,3 +23,35 @@ class InventoryManager:
         elif unit == "L":
             return pd.Series([qty * 1000.0, "ml"])
         return pd.Series([qty, unit])
+
+    #데이터 추출 및 표 형태로 출력
+    def get_inventory(self):
+        try:
+            conn = psycopg2.connect(**self.db_params)
+            cur = conn.cursor()
+            query = "SELECT id, ingredient_name, quantity, unit, expiration_date " \
+            "FROM public.ingredients;"
+            cur.execute(query)
+            
+            rows = cur.fetchall()
+            colnames = [desc[0] for desc in cur.description]
+            cur.close()
+            conn.close()
+
+            df = pd.DataFrame(rows, columns=colnames)
+            df["quantity"] = pd.to_numeric(df["quantity"])
+            df["expiration_date"] = pd.to_datetime(df["expiration_date"])
+            df[["std_quantity", "std_unit"]] = df.apply(self.convert_units, axis=1)
+
+            current_date = datetime.now()
+            fresh_df = df[df["expiration_date"] >= current_date]
+
+            inventory_summary = fresh_df.groupby(["ingredient_name", "std_unit"])["std_quantity"].sum().reset_index()
+            inventory_summary = inventory_summary.rename(
+                columns={"ingredient_name": "재료명", "std_quantity": "총재고", "std_unit": "단위"})
+            inventory_summary = inventory_summary[["재료명", "총재고", "단위"]]
+            return inventory_summary 
+        #에러 출력
+        except Exception as e:
+            print(f"에러 발생: {e}")
+            return None
